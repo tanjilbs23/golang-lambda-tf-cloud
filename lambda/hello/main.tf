d6ae7789570d1a3cca040fca45ca8a623726da66
tanjilbs23 <tanjil.bs23@gmail.com>
2022-10-18 20:04:51 +0600

    testing linux commands

--- lambda/hello/main.tf
+++ lambda/hello/main.tf
@@ -12,7 +12,7 @@ resource "null_resource" "lambda_build" {
   #   })
   # }
   provisioner "local-exec" {
-    command = "cd ${path.module}; wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz; tar -xzf go1.19.2.linux-amd64.tar.gz -C .; pwd; ls -la;cd go; pwd"
+    command = "cd ${path.module}; wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz; tar -xzf go1.19.2.linux-amd64.tar.gz -C .; pwd; ls -la;cd go/bin; pwd; go version"
     # command = "cat /etc/*release* go build -o ../bin/handler"
   }
 }
