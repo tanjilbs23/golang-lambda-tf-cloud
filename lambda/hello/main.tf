8826ef0098118d720fdeda85d338da24aa3cdac6
tanjilbs23 <tanjil.bs23@gmail.com>
2022-10-19 11:40:13 +0600

    build test for golang lambda & deploy using module

--- lambda/hello/main.tf
+++ lambda/hello/main.tf
@@ -1,58 +1,58 @@
 resource "null_resource" "lambda_build" {
-  # triggers = {
-  #   always_run = "${timestamp()}"
-  # }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
   # triggers = {
   #   on_every_apply = uuid()
   # }
-  triggers = {
-    file_hashes = jsonencode({
-      for fn in fileset("${path.module}/src", "**") :
-      fn => filesha256("${path.module}/src/${fn}")
-    })
-  }
+  # triggers = {
+  #   file_hashes = jsonencode({
+  #     for fn in fileset("${path.module}/src", "**") :
+  #     fn => filesha256("${path.module}/src/${fn}")
+  #   })
+  # }
   provisioner "local-exec" {
     command = "cd ${path.module};wget https://go.dev/dl/go1.19.2.linux-amd64.tar.gz;tar -xzf go1.19.2.linux-amd64.tar.gz -C .;cd go/bin;export PATH=$PATH:$(pwd);go version;cd ../../src;go build -o ../bin/handler;cd ..;ls -la;pwd;cd bin;ls -la;pwd;"
 
   }
 }
 
-# data "archive_file" "lambda_go_zip" {
+data "archive_file" "lambda_go_zip" {
 
-#   type        = "zip"
-#   source_file = "${path.module}/bin/handler"
-#   output_path = "${path.module}/bin/handler.zip"
+  type        = "zip"
+  source_file = "${path.module}/bin/handler"
+  output_path = "${path.module}/bin/handler.zip"
 
-#   depends_on = [
-#     null_resource.lambda_build
-#   ]
-# }
+  depends_on = [
+    null_resource.lambda_build
+  ]
+}
 
 
-# module "lambda_function" {
-#   source        = "terraform-aws-modules/lambda/aws"
-#   function_name = "handler"
-#   description   = "testing go function"
-#   handler       = "handler.lambda_handler"
-#   runtime       = "go1.x"
+module "lambda_function" {
+  source        = "terraform-aws-modules/lambda/aws"
+  function_name = "handler"
+  description   = "testing go function"
+  handler       = "handler.lambda_handler"
+  runtime       = "go1.x"
 
-#   create_package         = false
-#   local_existing_package = "${path.module}/bin/handler.zip"
+  create_package         = false
+  local_existing_package = "${path.module}/bin/handler.zip"
 
-#   trusted_entities = [
-#     {
-#       type = "Service",
-#       identifiers = [
-#         "appsync.amazonaws.com"
-#       ]
-#     }
-#   ]
+  trusted_entities = [
+    {
+      type = "Service",
+      identifiers = [
+        "appsync.amazonaws.com"
+      ]
+    }
+  ]
 
-#   tags = {
-#     Name = var.tags
-#   }
+  tags = {
+    Name = var.tags
+  }
 
-#   depends_on = [
-#     data.archive_file.lambda_go_zip
-#   ]
-# }
+  depends_on = [
+    data.archive_file.lambda_go_zip
+  ]
+}
